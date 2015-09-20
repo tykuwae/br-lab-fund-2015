b823babfece2e0759231e04e2cb50f16c7ce7185
Saulo Furuta <saulotoshi@gmail.com>
2015-09-20 16:03:48 -0300

    added routines

---
+++ Aula03/T5G08A03E02_const.asm
@@ -0,0 +1,21 @@
+;***** constantes *****
+h0001 >
+h0002 >
+h0100 >
+hFF00 >
+
+;***** variaveis para PACK() *****
+packA   >
+packB   >  
+  & /0000
+  ;***** constantes *****
+  h0001 K /0001 
+  h0002 K /0002  
+  h0100 K /0100  
+  hFF00 K /FF00    
+
+  ;***** variaveis para PACK() *****
+  packA $ /0001 ;entrada 1 de PACK   
+  packB $ /0001 ;entrada 2 de PACK        
+
+# VARIAVEIS
---
+++ Aula03/T5G08A03E02_main.asm
@@ -0,0 +1,19 @@
+PACK    <
+packA   <
+packB   < 
+& /0000 ;ANTES ERA @ /0000. SUBSTITUÍDO POR & /0000 APÓS COMENTÁRIO EM AULA SOBRE BUG.
+
+MAIN  JP  INICIO
+    A K /0010
+    B K /0064
+    SAIDA $ /0001
+
+INICIO  LD  A
+    MM  packA
+    LD  B
+    MM  packB
+    SC  PACK
+    MM  SAIDA
+FIM   HM  FIM
+
+# MAIN  
