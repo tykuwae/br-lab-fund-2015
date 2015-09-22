972673b307686d0d4b15c6384f62cffe40cd6e99
Saulo Furuta <saulotoshi@gmail.com>
2015-09-22 14:02:06 -0300

    extracted the chtoi in routines, const and main

--- Aula03/Exercicio02/T5G08A03E02_const.asm
+++ Aula03/Exercicio02/T5G08A03E02_const.asm
@@ -2,6 +2,12 @@
 h0000 >
 h0001 >
 h0002 >
+h0010 >
+h0030 >
+h0037 >
+h003A >
+h0047 >
+h1000 >
 h0100 >
 hFF00 >
 hFFFF >
@@ -18,7 +24,13 @@ unpackA >
 unpackB >
 temp >
 
-
+;***** variaveis para CHTOI() TODO extrair *****
+chtoiA  >
+chtoiB  >
+chtoiWordA >
+chtoiWordB >
+chtoiWordC >
+chtoiWordD >
 
 & /0000
 ;***** constantes *****
@@ -29,6 +41,14 @@ h0100 K /0100
 hFF00 K /FF00
 AmLoad    K /8000
 AmDownload k /9000
+h0030 K /0030
+h0037 K /0037 
+h003A K /003A 
+h0047 K /0047 
+h1000 K /1000
+h0010 K /0010
+hFFFF K /FFFF
+
 
 ;***** variaveis para PACK() *****
 packA $ /0001 ;entrada 1 de PACK   
@@ -39,5 +59,14 @@ unpackA $ /0001
 unpackB $ /0001
 temp $ /0001
 
+;***** variaveis para CHTOI() TODO extrair *****
+chtoiA  $ /0001 ; /3132
+chtoiB  $ /0001 ; /3334
+
+chtoiWordA $ /0001 ;/0031
+chtoiWordB $ /0001 ;/0032
+chtoiWordC $ /0001 ;/0033
+chtoiWordD $ /0001 ;/0034
+chtoiFINAL $ /0001
 
 # VARIAVEIS
--- Aula03/Exercicio02/T5G08A03E02_rotinas.asm
+++ Aula03/Exercicio02/T5G08A03E02_rotinas.asm
@@ -2,6 +2,12 @@
 h0000 <
 h0001 <
 h0002 <
+h0010 <
+h0030 <
+h0037 <
+h003A <
+h0047 <
+h1000 <
 h0100 <
 hFF00 <
 hFFFF <
@@ -18,6 +24,15 @@ UNPACK  >
 unpackA <
 unpackB <
 temp <
+
+;***** variaveis para CHTOI() TODO extrair *****
+CHTOI >
+chtoiA  <
+chtoiB  <
+chtoiWordA <
+chtoiWordB <
+chtoiWordC <
+chtoiWordD <
 
 & /0000
 ;**************************** PACK() ***************************************;
@@ -47,5 +62,88 @@ casoNeg2  - hFF00
           MM unpackA
 fimSubRotina  RS UNPACK
 
+;*************************** CHTOI() **************************************;
+
+CHTOI $ /0001
+          LD  chtoiA
+          SC UNPACK
+          LD unpackA
+          MM chtoiWordA
+          - h0047
+          JN continua1
+          JP errorChtoi
+continua1 LD chtoiWordA
+          - h003A
+          JN isNumber
+          LD chtoiWordA
+          - h0037
+shift     * h1000
+          MM chtoiWordA
+          JP nextWord
+isNumber  LD chtoiWordA
+          - h0030
+          JP shift
+;Proxima palavra
+nextWord  LD unpackB
+            MM chtoiWordB
+            - h0047
+            JN continua2
+            JP errorChtoi
+continua2   LD chtoiWordB
+            - h003A
+            JN isNumber2
+            LD chtoiWordB
+            - h0037
+shift2     * h0100
+            MM chtoiWordB
+            JP nextWord2
+isNumber2  LD chtoiWordB
+            - h0030
+            JP shift2
+;Proxima palavra
+ nextWord2  LD chtoiB
+            SC UNPACK
+            LD unpackA
+            MM chtoiWordC
+            - h0047
+            JN continua3
+            JP errorChtoi
+continua3   LD chtoiWordC
+            - h003A
+            JN isNumber3
+            LD chtoiWordC
+            - h0037
+shift3      * h0010
+            MM chtoiWordC
+            JP nextWord3
+isNumber3   LD chtoiWordC
+            - h0030
+            JP shift3
+;Proxima palavra
+nextWord3   LD unpackB
+            MM chtoiWordD
+            - h0047
+            JN continua4
+            JP errorChtoi
+continua4   LD chtoiWordD
+            - h003A
+            JN isNumber4
+            LD chtoiWordD
+            - h0037
+shift4      MM chtoiWordD
+            JP addAll
+isNumber4   LD chtoiWordD
+            - h0030
+            JP shift4
+;Agrupar os nibbles
+addAll      LD chtoiWordA
+            + chtoiWordB
+            + chtoiWordC
+            + chtoiWordD
+endChtoi    RS CHTOI
+
+errorChtoi  LD hFFFF
+            JP endChtoi
+  
 # PACK
 
